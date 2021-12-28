7aa529e5dfcee708106429e7515350846fab292b
tkuzynow <tomasz.kuzynowski@gmail.com>
2021-12-28 14:57:25 +0100

    init db scripts

--- src/main/resources/db/changelog/changeset/0001_initsql/initTables.sql
+++ src/main/resources/db/changelog/changeset/0001_initsql/initTables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tenantservice.`tenant` (
   `id` bigint(21) NOT NULL,
   `name` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
-  `subdomain` unique varchar(255) NOT NULL,
+  `subdomain` varchar(255) NOT NULL,
   `licensing_allowed_users` int COLLATE utf8_unicode_ci,
   `theming_logo` longtext,
   `theming_favicon` longtext,
@@ -10,9 +10,13 @@ CREATE TABLE tenantservice.`tenant` (
   `content_impressum` longtext,
   `content_claim` varchar(1024),
   `create_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP(),
-  `update_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP()
+  `update_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE tenantservice.`tenant`
+ADD CONSTRAINT unique_subdomain UNIQUE (subdomain);
+
 CREATE SEQUENCE tenantservice.sequence_tenant
 INCREMENT BY 1
 MINVALUE = 0

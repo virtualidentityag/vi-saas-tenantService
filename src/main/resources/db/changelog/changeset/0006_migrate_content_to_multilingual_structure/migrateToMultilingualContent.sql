c1a2f4121b37293bcaf5255fdc7556122d01a6be
tkuzynow <tomasz.kuzynowski@gmail.com>
2022-12-06 12:15:16 +0100

    chore:added migration scripts

---
+++ src/main/resources/db/changelog/changeset/0006_migrate_content_to_multilingual_structure/migrateToMultilingualContent.sql
@@ -0,0 +1,6 @@
+UPDATE `tenantservice`.`tenant` SET content_privacy = concat("[{'de': '", content_privacy, "'}]");
+UPDATE `tenantservice`.`tenant` SET content_termsandconditions = concat("[{'de': '", content_termsandconditions, "'}]");
+UPDATE `tenantservice`.`tenant` SET content_impressum = concat("[{'de': '", content_impressum, "'}]");
+
+
+

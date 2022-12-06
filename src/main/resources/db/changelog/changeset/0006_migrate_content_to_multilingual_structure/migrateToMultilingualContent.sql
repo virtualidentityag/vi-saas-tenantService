0c097497afc1381d520744c9277207f53c1134bd
tkuzynow <tomasz.kuzynowski@gmail.com>
2022-12-06 14:18:30 +0100

    chore: apply fix in migration scripts

--- src/main/resources/db/changelog/changeset/0006_migrate_content_to_multilingual_structure/migrateToMultilingualContent.sql
+++ src/main/resources/db/changelog/changeset/0006_migrate_content_to_multilingual_structure/migrateToMultilingualContent.sql
@@ -1,6 +1,10 @@
-UPDATE `tenantservice`.`tenant` SET content_privacy = concat("[{'de': '", content_privacy, "'}]");
-UPDATE `tenantservice`.`tenant` SET content_termsandconditions = concat("[{'de': '", content_termsandconditions, "'}]");
-UPDATE `tenantservice`.`tenant` SET content_impressum = concat("[{'de': '", content_impressum, "'}]");
+ALTER TABLE `tenant`
+MODIFY `content_claim` varchar(1024) COLLATE 'utf8_unicode_ci' NULL
+
+UPDATE `tenantservice`.`tenant` SET content_privacy = concat('[{"de": "', content_privacy, '"}]');
+UPDATE `tenantservice`.`tenant` SET content_termsandconditions = concat('[{"de": "', content_termsandconditions, '"}]');
+UPDATE `tenantservice`.`tenant` SET content_impressum = concat('[{"de": "', content_impressum, '"}]');
+UPDATE `tenantservice`.`tenant` SET content_claim = concat('[{"de": "', content_claim, '"}]');
 
 
 

b6552c1e5ae766c4a65fb4dfb6f4883843990e1a
tkuzynow <tomasz.kuzynowski@gmail.com>
2022-12-06 11:01:22 +0100

    fix: permissions for agency admin

--- src/main/java/com/vi/tenantservice/api/authorisation/UserRole.java
+++ src/main/java/com/vi/tenantservice/api/authorisation/UserRole.java
@@ -1,16 +1,21 @@
 package com.vi.tenantservice.api.authorisation;
 
-import java.util.Arrays;
-import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Getter
 public enum UserRole {
 
   TENANT_ADMIN("tenant-admin"),
-  SINGLE_TENANT_ADMIN("single-tenant-admin");
+  SINGLE_TENANT_ADMIN("single-tenant-admin"),
+
+  RESTRICTED_AGENCY_ADMIN("restricted-agency-admin"),
+
+  NON_PRIVILIGED_ADMIN("non-priviliged-admin");
 
   private final String value;
 

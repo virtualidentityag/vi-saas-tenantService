3ee0e25aab129afb58f87546e0e437599042d2fa
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-01-24 13:00:44 +0100

    fix: add default headers

--- src/main/java/com/vi/tenantservice/api/service/consultingtype/ConsultingTypeService.java
+++ src/main/java/com/vi/tenantservice/api/service/consultingtype/ConsultingTypeService.java
@@ -77,6 +77,7 @@ public class ConsultingTypeService {
     var consultingTypeControllerApi =
         consultingTypeServiceApiControllerFactory.createControllerApi();
 
+    addDefaultHeaders(consultingTypeControllerApi.getApiClient());
     return consultingTypeControllerApi.getFullConsultingTypeByTenantId(tenantId);
   }
 

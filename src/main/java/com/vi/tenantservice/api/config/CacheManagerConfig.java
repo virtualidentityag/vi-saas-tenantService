6565efd3746df05ef978286d607821c75259beeb
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-07-14 11:28:49 +0200

    fix: ehcache configuration

--- src/main/java/com/vi/tenantservice/api/config/CacheManagerConfig.java
+++ src/main/java/com/vi/tenantservice/api/config/CacheManagerConfig.java
@@ -5,44 +5,4 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableCaching
-public class CacheManagerConfig {
-
-  //  public static final String APPLICATION_SETTINGS_CACHE = "applicationSettingsCache";
-  //
-  //  @Value("${cache.applicationsettings.configuration.maxEntriesLocalHeap}")
-  //  private long applicationSettingsMaxEntriesLocalHeap;
-  //
-  //  @Value("${cache.applicationsettings.configuration.eternal}")
-  //  private boolean applicationSettingsEternal;
-  //
-  //  @Value("${cache.applicationsettings.configuration.timeToIdleSeconds}")
-  //  private long applicationSettingsTimeToIdleSeconds;
-  //
-  //  @Value("${cache.applicationsettings.configuration.timeToLiveSeconds}")
-  //  private long applicationSettingsTimeToLiveSeconds;
-  //
-  //  @Bean
-  //  public CacheManager cacheManager() {
-  //    return new EhCacheCacheManager(ehCacheManager());
-  //  }
-  //
-  //  @Bean(destroyMethod = "shutdown")
-  //  public CacheManager ehCacheManager() {
-  //    var config = new Configuration();
-  //
-  //    EhcacheManager manager = new EhcacheManager(buildApplicationSettingsCacheConfiguration());
-  //
-  //    config.addCache();
-  //    return CacheManager.newInstance(config);
-  //  }
-
-  //  private Configuration buildApplicationSettingsCacheConfiguration() {
-  //    var topicCacheConfiguration = new Configuration();
-  //    topicCacheConfiguration.setName(APPLICATION_SETTINGS_CACHE);
-  //    topicCacheConfiguration.setMaxEntriesLocalHeap(applicationSettingsMaxEntriesLocalHeap);
-  //    topicCacheConfiguration.setEternal(applicationSettingsEternal);
-  //    topicCacheConfiguration.setTimeToIdleSeconds(applicationSettingsTimeToIdleSeconds);
-  //    topicCacheConfiguration.setTimeToLiveSeconds(applicationSettingsTimeToLiveSeconds);
-  //    return topicCacheConfiguration;
-  //  }
-}
+public class CacheManagerConfig {}
